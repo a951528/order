8321462283995e821130ddef27fec360b99f6d36
Wei <a951528@gmail.com>
2022-04-29 12:22:22 +0800

    期中第二題

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,16 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
+    //int shipFeed = delivery.price; <<<不要寫這樣謝謝，寫在下面ㄉ建構子方法裡!!
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed= delivery.price;
+    }
+
+    public int total() {
+        return amount+shipFeed;
     }
 }
--- src/com/fju/order/Seven11Delivery.java
+++ src/com/fju/order/Seven11Delivery.java
@@ -1,8 +1,10 @@
 package com.fju.order;
 
 public class Seven11Delivery extends Delivery {
+
     public Seven11Delivery() {
         name = "7-11";
         price = 60;
     }
+
 }
